8fd75a0f0eecdd80e9e41437e1abb06b763473c3
Eluchini <eluchini@outlook.com>
2025-08-30 20:34:00 -0300

    refactor(api): convert callback functions to async/await for better readability. Prev to functional tests.

--- routes/api.js
+++ routes/api.js
@@ -17,7 +17,7 @@ module.exports = function (app) {
     .get(function (req, res){
       //response will be array of book objects
       //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
-      
+
       /* Book.find({}, (err, books) => {
         if (err) return res.status(500).send(err);
         res.json(books.map(book => ({
@@ -84,73 +84,53 @@ module.exports = function (app) {
 
 
   app.route('/api/books/:id')
-    .get(function (req, res){
-      let bookid = req.params.id;
+    .get(async function (req, res) {
+      const bookid = req.params.id;
       //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
-      /* Book.findById(bookid, (err, book) => {
-        if (err) return res.status(500).send('error finding book');
-        if (!book) return res.send('no book exists');
+      try {
+        const book = await Book.findById(bookid);
+        if (!book) {
+          return res.send('no book exists');
+        }
         res.json({ _id: book._id, title: book.title, comments: book.comments });
-      }); */
-      Book.findById(bookid)
-        .then(book => {
-          if (!book) return res.send('no book exists');
-          res.json({ _id: book._id, title: book.title, comments: book.comments });
-        })
-        .catch(err => {
-          console.error(`Error finding book: ${err}`);
-          res.status(500).send('error finding book');
-        });
+      } catch (err) {
+        console.error(`Error finding book: ${err}`);
+        res.status(500).send('error finding book');
+      }
     })
-    
-    .post(function(req, res){
-      let bookid = req.params.id;
-      let comment = req.body.comment;
+
+    .post(async function (req, res) {
+      const bookid = req.params.id;
+      const comment = req.body.comment;
       //json res format same as .get
       if (!comment) return res.send('missing required field comment');
-
-      /* Book.findById(bookid, (err, book) => {
-        if (err) return res.status(500).send('error finding book');
-        if (!book) return res.send('no book exists');
+      try {
+        const book = await Book.findById(bookid);
+        if (!book) {
+          return res.send('no book exists');
+        }
         book.comments.push(comment);
-        book.save((err, updatedBook) => {
-          if (err) return res.status(500).send('error saving comment');
-          res.json({ _id: updatedBook._id, title: updatedBook.title, comments: updatedBook.comments });
-        });
-      }); */
-      Book.findById(bookid)
-        .then(book => {
-          if (!book) return res.send('no book exists');
-          book.comments.push(comment);
-          return book.save();
-        })
-        .then(updatedBook => {
-          res.json({ _id: updatedBook._id, title: updatedBook.title, comments: updatedBook.comments });
-        })
-        .catch(err => {
-          console.error(`Error saving comment: ${err}`);
-          res.status(500).send('error saving comment');
-        });
+        const updatedBook = await book.save();
+        res.json({ _id: updatedBook._id, title: updatedBook.title, comments: updatedBook.comments });
+      } catch (err) {
+        console.error(`Error saving comment: ${err}`);
+        if (!res.headersSent) res.status(500).send('error saving comment');
+      }
     })
-    
-    .delete(function(req, res){
-      let bookid = req.params.id;
-      //if successful response will be 'delete successful'
 
-      /* Book.findByIdAndDelete(bookid, (err, book) => {
-        if (err) return res.status(500).send('error deleting book');
-        if (!book) return res.send('no book exists');
+    .delete(async function (req, res) {
+      const bookid = req.params.id;
+      //if successful response will be 'delete successful'
+      try {
+        const book = await Book.findByIdAndDelete(bookid);
+        if (!book) {
+          return res.send('no book exists');
+        }
         res.send('delete successful');
-      }); */
-      Book.findByIdAndDelete(bookid)
-        .then(book => {
-          if (!book) return res.send('no book exists');
-          res.send('delete successful');
-        })
-        .catch(err => {
-          console.error(`Error deleting book: ${err}`);
-          res.status(500).send('error deleting book');
-        });
+      } catch (err) {
+        console.error(`Error deleting book: ${err}`);
+        if (!res.headersSent) res.status(500).send('error deleting book');
+      }
     });
   
 };
