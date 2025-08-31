998e8ca06c08c0a1ccf0bebf162f59d4bbfca23d
Eluchini <eluchini@outlook.com>
2025-08-31 00:18:20 -0300

    style(tests): standardize quotes and formatting in functional tests for consistency- That's one down.

--- routes/api.js
+++ routes/api.js
@@ -1,69 +1,60 @@
 /*
-*
-*
-*       Complete the API routing below
-*       
-*       
-*/
+ *
+ *
+ *       Complete the API routing below
+ *
+ *
+ */
 
-'use strict';
+"use strict";
 
-const Book = require('../models/Book');
+const Book = require("../models/Book");
 
 module.exports = function (app) {
-  
-
-  app.route('/api/books')
-    .get(function (req, res){
+  app
+    .route("/api/books")
+    .get(function (req, res) {
       //response will be array of book objects
       //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
 
-      /* Book.find({}, (err, books) => {
-        if (err) return res.status(500).send(err);
-        res.json(books.map(book => ({
-          _id: book._id,
-          title: book.title,
-          commentcount: book.comments.length
-        })));
-      }); */
-
       Book.find({})
-        .then(books => {
-          res.json(books.map(book => ({
-            _id: book._id,
-            title: book.title,
-            commentcount: book.comments.length
-          })));
+        .then((books) => {
+          res.json(
+            books.map((book) => ({
+              _id: book._id,
+              title: book.title,
+              commentcount: book.comments.length,
+            }))
+          );
         })
-        .catch(err => {
+        .catch((err) => {
           console.error(`Error finding books: ${err}`);
-          res.status(500).send('error finding books');
+          res.status(500).send("error finding books");
         });
-
     })
-    
-    .post(function (req, res){
+
+    .post(function (req, res) {
       let title = req.body.title;
 
-      if(!title){
-        return res.send('missing required field title');
+      if (!title) {
+        return res.send("missing required field title");
       }
 
       //response will contain new book object including atleast _id and title
       //console.log(`New book created: ${JSON.stringify(req.body)}`);
       const newBook = new Book({ title, comments: [] });
-      newBook.save()
-        .then(savedBook => {
+      newBook
+        .save()
+        .then((savedBook) => {
           res.json({ _id: savedBook._id, title: savedBook.title });
         })
-        .catch(err => {
+        .catch((err) => {
           console.error(`Error saving book: ${err}`);
-          res.status(500).send('error saving book');
+          res.status(500).send("error saving book");
         });
-
     })
-    
-    .delete(function(req, res){
+
+    .delete(function (req, res) {
       //if successful response will be 'complete delete successful'
 
       /* Book.deleteMany({}, (err) => {
@@ -73,29 +64,28 @@ module.exports = function (app) {
 
       Book.deleteMany({})
         .then(() => {
-          res.send('complete delete successful');
+          res.send("complete delete successful");
         })
-        .catch(err => {
+        .catch((err) => {
           console.error(`Error deleting books: ${err}`);
-          res.status(500).send('error deleting books');
+          res.status(500).send("error deleting books");
         });
     });
 
-
-
-  app.route('/api/books/:id')
+  app
+    .route("/api/books/:id")
     .get(async function (req, res) {
       const bookid = req.params.id;
       //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
       try {
         const book = await Book.findById(bookid);
         if (!book) {
-          return res.send('no book exists');
+          return res.send("no book exists");
         }
         res.json({ _id: book._id, title: book.title, comments: book.comments });
       } catch (err) {
         console.error(`Error finding book: ${err}`);
-        res.status(500).send('error finding book');
+        res.status(500).send("error finding book");
       }
     })
 
@@ -103,18 +93,22 @@ module.exports = function (app) {
       const bookid = req.params.id;
       const comment = req.body.comment;
       //json res format same as .get
-      if (!comment) return res.send('missing required field comment');
+      if (!comment) return res.send("missing required field comment");
       try {
         const book = await Book.findById(bookid);
         if (!book) {
-          return res.send('no book exists');
+          return res.send("no book exists");
         }
         book.comments.push(comment);
         const updatedBook = await book.save();
-        res.json({ _id: updatedBook._id, title: updatedBook.title, comments: updatedBook.comments });
+        res.json({
+          _id: updatedBook._id,
+          title: updatedBook.title,
+          comments: updatedBook.comments,
+        });
       } catch (err) {
         console.error(`Error saving comment: ${err}`);
-        if (!res.headersSent) res.status(500).send('error saving comment');
+        if (!res.headersSent) res.status(500).send("error saving comment");
       }
     })
 
@@ -124,13 +118,12 @@ module.exports = function (app) {
       try {
         const book = await Book.findByIdAndDelete(bookid);
         if (!book) {
-          return res.send('no book exists');
+          return res.send("no book exists");
         }
-        res.send('delete successful');
+        res.send("delete successful");
       } catch (err) {
         console.error(`Error deleting book: ${err}`);
-        if (!res.headersSent) res.status(500).send('error deleting book');
+        if (!res.headersSent) res.status(500).send("error deleting book");
       }
     });
-  
 };
